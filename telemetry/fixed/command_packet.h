3cba2d23434ef4de7b37bef62efabe3c062c711b
Jeff <jeffreywjlim@gmail.com>
2025-11-20 18:17:00 -0500

    better command includes null

--- telemetry/fixed/command_packet.h
+++ telemetry/fixed/command_packet.h
@@ -1,9 +1,10 @@
 #include <cstdint>
 
-
+// Command string the char array is null terminated so we can send 2 letter commands
+// e.g po\0 p1\0 p2\0
 typedef struct __attribute__((__packed__)) command_packet_data {
     uint8_t command_id;
-    char command_string[2];
+    char command_string[3];
 } command_packet_data;
 
 typedef union command_packet {
