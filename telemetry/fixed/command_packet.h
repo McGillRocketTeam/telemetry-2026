2c03570bc4ae2af2f7d3e04bd4ddb2c1f68596b8
Jeff <jeffreywjlim@gmail.com>
2026-03-02 17:00:34 -0500

    added new extended packet

--- telemetry/fixed/command_packet.h
+++ telemetry/fixed/command_packet.h
@@ -5,10 +5,25 @@
 
 typedef struct __attribute__((__packed__)) command_packet_data {
     uint8_t command_id;
-    char command_string[5];
+    char command_string[6];
 } command_packet_data;
 
 typedef union command_packet {
     command_packet_data data;
     uint8_t bytes[sizeof(command_packet_data)];
 } command_packet;
+
+static_assert(sizeof(command_packet_data) % 4 == 3,
+    "command_packet_data must be ensure 4 byte alignment in command_packet_extended_data");
+
+// Be very careful, we need the float args to start 4byte aligned
+typedef struct __attribute__((__packed__)) command_packet_extended_data {
+    command_packet base;
+    uint8_t argc;
+    float args[3];
+} command_packet_extended_data;
+
+typedef union command_packet_extended {
+    command_packet_extended_data data;
+    uint8_t bytes[sizeof(command_packet_extended_data)];
+} command_packet_extended;
